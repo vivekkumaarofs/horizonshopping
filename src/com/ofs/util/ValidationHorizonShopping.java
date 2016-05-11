7edd56c2b0337f3d8ee41bfa9a6d5308185ec35b
Vivek Kumaar U <vivekkumaar.ulagnathan@object-frontier.com>
2016-05-11 17:01:51 +0530

    changes in validation regex

--- src/com/ofs/util/ValidationHorizonShopping.java
+++ src/com/ofs/util/ValidationHorizonShopping.java
@@ -11,7 +11,7 @@ public class ValidationHorizonShopping {
 
 	public void validateUserDetails(User user) {
 
-		String passwordpattern= "((?=.*[@!#$%])(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,22})";
+		String passwordpattern= "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
 		String emailidpattern ="^[A-Za-z0-9+_.-]+@(.+)$";
 		String namepattern ="^[A-Za-z]+";
 		
